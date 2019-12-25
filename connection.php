49fe048d1788ea9a419283eaade2a9804b648329
parthvora2103 <parthv.corepix@gmail.com>
2019-12-25 18:11:51 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,16 +7,16 @@ if (mysqli_connect_errno()) {
   exit;
 }
 
-mysqli_select_db($con, "login");
+mysqli_select_db($con, "login"); // add database name
 
 
-$sql = "SELECT * FROM student";
+$sql = "SELECT * FROM student";//add table name 
 $result = $con->query($sql);
 
 if ($result->num_rows > 0) {
     
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["stud_id"]. " - Name: " . $row["stud_name"]. " " . $row["roll_no"]. "<br>";
+        echo "id: " . $row["stud_id"]. " - Name: " . $row["stud_name"]. " " . $row["roll_no"]. "<br>"; // add all attributes 
     }
 } else {
     echo "0 results";
